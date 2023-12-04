aba9e4b7e2cb764d5189199d406cc52391180447
Aman Shah <aman.j.shah@gmail.com>
2023-12-04 10:50:32 +0000

    initialise files for day 4

--- day2.py
+++ day2.py
@@ -3,8 +3,10 @@ import os
 
 day2 = open(os.path.join("input-data-2023", "23-2.txt"))
 
+
 def getResult(dayTwoFile):
     return sum(getPowerOfSet(line) for line in dayTwoFile)
+
 
 def getPowerOfSet(line):
     game = line.split(":")[1].strip().split("; ")
@@ -12,12 +14,12 @@ def getPowerOfSet(line):
     for gameSet in game: adjustMinimumNumbersForSet(minimumNumbers, gameSet)
     return math.prod(minimumNumbers.values())
 
+
 def adjustMinimumNumbersForSet(minimumNumbers, gameSet):
     colourNumbers = gameSet.split(",")
     for unknownColour in colourNumbers:
         number, colour = unknownColour.split()
         minimumNumbers[colour] = max(int(number), minimumNumbers[colour])
 
-res = getResult(day2)
 
-print(res)
+print(getResult(day2))
--- day3.py
+++ day3.py
@@ -3,8 +3,10 @@ import os
 
 day3 = open(os.path.join("input-data-2023", "23-3.txt"))
 
+
 def positionIsInvalid(partNumbers, schematic, idxI, idxJ, seen):
     return len(partNumbers)>2 or idxI >= len(schematic) or idxJ >= len(schematic[0]) or seen[idxI][idxJ] or not schematic[idxI][idxJ].isdigit()
+
 
 def getPartNumber(schematic, seen, idxI, idxJ):
     partNumber, shift, position = '0', 0, schematic[idxI][idxJ]
@@ -20,6 +22,7 @@ def getPartNumber(schematic, seen, idxI, idxJ):
         position = schematic[idxI][idxJ+shift]
     return partNumber
 
+
 def getPartNumbersForSymbol(deltas, schematic, seen, i, j):
     partNumbers = []
     for di,dj in deltas:
@@ -28,6 +31,7 @@ def getPartNumbersForSymbol(deltas, schematic, seen, i, j):
         partNumber = getPartNumber(schematic, seen, idxI, idxJ)
         partNumbers.append(int(partNumber))
     return partNumbers
+
 
 def getResult(day3):
     gearRatioSum = 0
