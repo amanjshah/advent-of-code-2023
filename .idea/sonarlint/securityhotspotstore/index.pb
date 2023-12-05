e368d05c30f7292f694c9d6b114696d25f5ed531
Aman Shah <aman.j.shah@gmail.com>
2023-12-05 17:53:01 +0000

    add day 5 solution

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+H
+input-data-2023/23-4.txt,a/0/a0eda34beee0c50ef882c0cc98c39f00598fe359
---
+++ day5.py
@@ -0,0 +1,5 @@
+import os
+
+day5 = open(os.path.join("input-data-2023", "23-5.txt"))
+for line in day4:
+    print(line)
