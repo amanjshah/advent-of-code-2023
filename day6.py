f33effd83c39912b629b4a53c061eb50127ff014
Aman Shah <aman.j.shah@gmail.com>
2023-12-06 09:16:08 +0000

    minimise time complexity

--- day6.py
+++ day6.py
@@ -1,4 +1,5 @@
 import os
+import math
 
 
 def getValues():
@@ -6,22 +7,10 @@ def getValues():
     return (int(''.join(char for char in day6[i] if char.isdigit())) for i in range(2))
 
 
-def firstWinningButtonDuration(time, distanceToBeat):
-    # binary search
-    left, right = 1, time-1
-    while left < right:
-        charge = left + (right - left) // 2
-        if distanceToBeat < charge*(time-charge):
-            right = charge
-        else:
-            left = charge + 1
-    return left
-
-
 def getResult():
     time, distanceToBeat = getValues()
-    print(firstWinningButtonDuration(time, distanceToBeat))
-    return time - 2*firstWinningButtonDuration(time, distanceToBeat) + 1
+    discriminantSqrt = (time**2-4*distanceToBeat)**0.5
+    return math.ceil((time+discriminantSqrt)/2) - math.ceil((time-discriminantSqrt)/2)
 
 
 print(getResult())
