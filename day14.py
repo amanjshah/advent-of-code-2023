700777e923fe59cb7933f8ef3e196f9f8a503a0e
Aman Shah <aman.j.shah@gmail.com>
2023-12-14 10:35:00 +0000

    update part 1

--- day14.py
+++ day14.py
@@ -1,26 +1,33 @@
 import os
 
-day14 = open(os.path.join("input-data-2023", "23-14.txt")).read().strip().split("\n")
-print(day14)
-roundedRocks = dict((col, [False for _ in range(len(day14[0]))]) for col in range(len(day14[0])))
-cubedRocks = dict((col, [False for _ in range(len(day14[0]))]) for col in range(len(day14[0])))
-for i in range(len(day14)):
-    for j in range(len(day14[0])):
-        if day14[i][j] == "O":
-            roundedRocks[j][i] = True
-        elif day14[i][j] == "#":
-            cubedRocks[j][i] = True
-load = 0
-for c in range(len(day14[0])):
-    if roundedRocks[c][0]:
-        load += len(day14)
-    for r in range(1, len(day14)):
-        if not roundedRocks[c][r]:
-            continue
-        pointer = r-1
-        while not (pointer < 0 or roundedRocks[c][pointer] or cubedRocks[c][pointer]):
-            pointer -= 1
-        pointer += 1
-        roundedRocks[c][r] = False
-        roundedRocks[c][pointer] = True
-        load += len(day14) - pointer
+data = [list(row) for row in open(os.path.join("input-data-2023", "23-14.txt")).read().strip().split("\n")]
+roundedRocks = dict((col, [False for _ in range(len(data[0]))]) for col in range(len(data[0])))
+cubedRocks = dict((col, [False for _ in range(len(data[0]))]) for col in range(len(data[0])))
+
+def rotate(data):
+    rotatedData = [['X' for _ in range(len(data))] for _ in range(len(data[0]))]
+    for i in range(len(data)):
+        for j in range(len(data[0])):
+            rotatedData[j][len(data) - i - 1] = data[i][j]
+    return rotatedData
+
+
+def pushUp(data):
+    load = 0
+    for j in range(len(data[0])):
+        if data[0][j] == "O":
+            load += len(data)
+        for i in range(1, len(data)):
+            if data[i][j] != "O":
+                continue
+            pointer = i - 1
+            while pointer >= 0 and data[pointer][j] == ".":
+                pointer -= 1
+            pointer += 1
+            data[i][j] = "."
+            data[pointer][j] = "O"
+            load += len(data) - pointer
+    return load, data
+
+
+print(pushUp(data))
