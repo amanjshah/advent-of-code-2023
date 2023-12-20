8501a85200e2cb92e7fe05eb44dee9704f4c4ec3
Aman Shah <aman.j.shah@gmail.com>
2023-12-20 21:16:51 +0000

    finish day 20

--- day20.py
+++ day20.py
@@ -8,18 +8,18 @@ def getData():
             open(os.path.join("input-data-2023", "23-20.txt")).read().strip().split("\n")]
     modulesMap = dict((mapping[0][1:], mapping[1].split(", ")) if mapping[0][0] in "%&"
                       else (mapping[0], mapping[1].split(", ")) for mapping in data)
-    flipflops = {module: False for module in set(map(lambda module: module[0][0] == "%" and module[0][1:], data))}
-    conjunctions = {module: [] for module in set(map(lambda module: module[0][0] == "&" and module[0][1:], data))}
-    flipflops.pop(False, None)
-    conjunctions.pop(False, None)
+    allFlipflops = {module: False for module in set(map(lambda module: module[0][0] == "%" and module[0][1:], data))}
+    allConjunctions = {module: [] for module in set(map(lambda module: module[0][0] == "&" and module[0][1:], data))}
+    allFlipflops.pop(False, None)
+    allConjunctions.pop(False, None)
     for module in modulesMap:
         for nextModule in modulesMap[module]:
-            if nextModule in conjunctions:
-                conjunctions[nextModule].append([module, False])
-    return modulesMap, flipflops, conjunctions
+            if nextModule in allConjunctions:
+                allConjunctions[nextModule].append([module, False])
+    return modulesMap, allFlipflops, allConjunctions
 
 
-MODULES_MAP, FLIPFLOPS, CONJUNCTIONS = getData()
+MODULES_MAP, flipflops, conjunctions = getData()
 CONJUNCTIONS_TO_SYNC = set(module for module in MODULES_MAP if "kz" in MODULES_MAP[module])
 
 
@@ -32,11 +32,14 @@ def addToQueue(queue, module, outputSignal, cycleLengths, buttonPushes):
         queue.append((nextModule, outputSignal, module))
 
 
-def addConjunctionToQueue(queue, module, previousModule, inputSignal, cycleLengths, buttonPushes):
-    for i in range(len(CONJUNCTIONS[module])):
-        if CONJUNCTIONS[module][i][0] == previousModule:
-            CONJUNCTIONS[module][i][1] = inputSignal
-    for inputModule, lastReceived in CONJUNCTIONS[module]:
+def updateConjunctions(inputSignal, module, previous):
+    for i in range(len(conjunctions[module])):
+        if conjunctions[module][i][0] == previous:
+            conjunctions[module][i][1] = inputSignal
+
+
+def addConjunctionToQueue(buttonPushes, cycleLengths, module, queue):
+    for inputModule, lastReceived in conjunctions[module]:
         if not lastReceived:
             addToQueue(queue, module, True, cycleLengths, buttonPushes)
             break
@@ -44,22 +47,23 @@ def addConjunctionToQueue(queue, module, previousModule, inputSignal, cycleLengt
         addToQueue(queue, module, False, cycleLengths, buttonPushes)
 
 
-def addFlipflopToQueue(queue, module, cycleLengths, buttonPushes):
-    FLIPFLOPS[module] = not FLIPFLOPS[module]
-    addToQueue(queue, module, FLIPFLOPS[module], cycleLengths, buttonPushes)
+def pushButton(buttonPushes, cycleLengths):
+    queue = deque([(node, False, "broadcaster") for node in MODULES_MAP["broadcaster"]])
+    while queue:
+        module, inputSignal, previous = queue.popleft()
+        if module in flipflops and not inputSignal:
+            flipflops[module] = not flipflops[module]
+            addToQueue(queue, module, flipflops[module], cycleLengths, buttonPushes)
+        elif module in conjunctions:
+            updateConjunctions(inputSignal, module, previous)
+            addConjunctionToQueue(buttonPushes, cycleLengths, module, queue)
 
 
 def getResult():
     cycleLengths, buttonPushes = {}, 0
     while len(CONJUNCTIONS_TO_SYNC) > len(cycleLengths):
         buttonPushes += 1
-        queue = deque([(node, False, "broadcaster") for node in MODULES_MAP["broadcaster"]])
-        while queue:
-            module, inputSignal, previous = queue.popleft()
-            if module in FLIPFLOPS and not inputSignal:
-                addFlipflopToQueue(queue, module, cycleLengths, buttonPushes)
-            elif module in CONJUNCTIONS:
-                addConjunctionToQueue(queue, module, previous, inputSignal, cycleLengths, buttonPushes)
+        pushButton(buttonPushes, cycleLengths)
     return lcm(*cycleLengths.values())
 
 
