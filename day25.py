b593430b9f933440254ee01f375a58eca6fd6245
Aman Shah <aman.j.shah@gmail.com>
2023-12-25 11:32:41 +0000

    add day 25 solution

--- day25.py
+++ day25.py
@@ -1,3 +1,22 @@
+import math
 import os
+import networkx as nx
 
-data = open(os.path.join("input-data-2023", "23-25.txt")).read().strip().split("\n")
+
+def createGraph():
+    data = [line.split(": ") for line in open(os.path.join("input-data-2023", "23-25.txt")).read().strip().split("\n")]
+    graph = nx.Graph()
+    for node, connectedNodes in data:
+        for connectedNode in (connectedNodes.split()):
+            graph.add_edge(node, connectedNode)
+            graph.add_edge(connectedNode, node)
+    return graph
+
+
+def getResult():
+    graph = createGraph()
+    graph.remove_edges_from(nx.minimum_edge_cut(graph))
+    return math.prod(map(len, nx.connected_components(graph)))
+
+
+print(getResult())
